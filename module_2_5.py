5be74851138ccb1ab064c95a62ec7ef697b9069d
Riji-ai <riji.dav@yandex.ru>
2024-08-11 16:10:41 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,14 @@
+n = [3, 4, 5]
+m = [2, 7, 9]
+value =[8, 6, 10]
+def get_matrix():
+    matrix = []
+for get_matrix in n, m :
+    for get_matrix in m, n:
+        print(get_matrix)
+matrix = [[get_matrix], [value]]
+print(matrix)
+
+
+
+
